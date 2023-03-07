ecb271924c7164636ae1f061fa9bdddb735890cf
Andrey Martin <jeffersonandreyma12@gmail.com>
2023-03-07 10:42:54 -0500

    Connecting HTML with JavaScript

---
+++ js/mokepon.js
@@ -0,0 +1,7 @@
+let buttonPetPlayer = document.getElementById("button-pet");
+
+function selectPetPlayer() {
+    alert('You select your pet');
+}
+
+buttonPetPlayer.addEventListener('click', selectPetPlayer);
