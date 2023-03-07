f376b477757e1d43bf5dea3f225500d51daa426f
Andrey Martin <jeffersonandreyma12@gmail.com>
2023-03-07 12:15:13 -0500

    addEventListener

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,23 @@
-let buttonPetPlayer = document.getElementById("button-pet");
-
-function selectPetPlayer() {
-    alert('You select your pet');
+function startGame() {
+    let buttonPetPlayer = document.getElementById("button-pet");
+    buttonPetPlayer.addEventListener("click", selectPetPlayer);
 }
 
-buttonPetPlayer.addEventListener('click', selectPetPlayer);
+function selectPetPlayer() {
+    let hipodoge = document.getElementById("hipodoge");
+    let capipepo = document.getElementById("capipepo");
+    let ratigueya = document.getElementById("ratigueya");
+
+    if (hipodoge.checked) {
+        alert("Your choose Hipodoge");
+    } else if (capipepo.checked) {
+        alert("Your choose Capipepo");
+    } else if (ratigueya.checked) {
+        alert("Your choose Ratigueya");
+    } else {
+        alert("You must select a pet ");
+    }
+}
+
+window.addEventListener("load", startGame);
+ 
