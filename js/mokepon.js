0f0c2c817d399a34c1b5f3089115a94dd4115350
Andrey Martin <jeffersonandreyma12@gmail.com>
2023-03-08 11:46:22 -0500

    Choose the player's pet and the enemy's pet

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,23 +1,39 @@
 function startGame() {
-    let buttonPetPlayer = document.getElementById("button-pet");
-    buttonPetPlayer.addEventListener("click", selectPetPlayer);
+    let buttonPetPlayer = document.getElementById('button-pet');
+    buttonPetPlayer.addEventListener('click', selectPetPlayer);
 }
 
 function selectPetPlayer() {
-    let hipodoge = document.getElementById("hipodoge");
-    let capipepo = document.getElementById("capipepo");
-    let ratigueya = document.getElementById("ratigueya");
+    let hipodoge = document.getElementById('hipodoge');
+    let capipepo = document.getElementById('capipepo');
+    let ratigueya = document.getElementById('ratigueya');
+
+    let spanPetPlayer = document.getElementById('pet-player');
 
     if (hipodoge.checked) {
-        alert("Your choose Hipodoge");
+        spanPetPlayer.innerHTML = 'Hipodoge';
     } else if (capipepo.checked) {
-        alert("Your choose Capipepo");
+        spanPetPlayer.innerHTML = 'Capipepo';
     } else if (ratigueya.checked) {
-        alert("Your choose Ratigueya");
+        spanPetPlayer.innerHTML = 'Ratigueya';
     } else {
         alert("You must select a pet ");
     }
+
+    selectPetEnemy();
 }
 
-window.addEventListener("load", startGame);
- 
+function selectPetEnemy() {
+    let attackRandom = random(1, 3);
+    let spanPetEnemy = document.getElementById('pet-enemy');
+
+    if (attackRandom == 1) spanPetEnemy.innerHTML = 'Hipodoge';
+    else if (attackRandom == 2) spanPetEnemy.innerHTML = 'Capipepo';
+    else if (attackRandom == 3) spanPetEnemy.innerHTML = 'Ratigueya';
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+window.addEventListener('load', startGame);
