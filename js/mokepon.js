4f6734aeecb5f09591ff9b2d72ef5294b51e62fe
Andrey Martin <jeffersonandreyma12@gmail.com>
2023-03-09 13:03:41 -0500

    Choose power player and enemy

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,29 +1,40 @@
 const Pets = {
-    Hipodoge: "Hipodoge",
-    Capipepo: "Capipepo",
-    Ratigueya: "Ratigueya",
-    Langostelvis: "Langostelvis",
-    Tucapalma: "Tucapalma",
-    Pydos: "Pydos",
+    Hipodoge: 'Hipodoge',
+    Capipepo: 'Capipepo',
+    Ratigueya: 'Ratigueya',
+    Langostelvis: 'Langostelvis',
+    Tucapalma: 'Tucapalma',
+    Pydos: 'Pydos',
+};
+
+const Powers = {
+    Pyro: 'Pyro',
+    Hydro: 'Hydro',
+    Geo: 'Geo',
+    Cryo: 'Cryo',
+    Electro: 'Electro',
 };
 
 let currentPetPlayer;
 let currentPetEnemy;
 
+let attackPlayer;
+let attackEnemy;
+
 function startGame() {
-    let buttonPetPlayer = document.getElementById("button-pet");
-    buttonPetPlayer.addEventListener("click", selectPetPlayer);
+    let buttonPetPlayer = document.getElementById('button-pet');
+    buttonPetPlayer.addEventListener('click', selectPetPlayer);
 }
 
 function selectPetPlayer() {
-    let hipodoge = document.getElementById("hipodoge");
-    let capipepo = document.getElementById("capipepo");
-    let ratigueya = document.getElementById("ratigueya");
-    let langostelvis = document.getElementById("langostelvis");
-    let tucapalma = document.getElementById("tucapalma");
-    let pydos = document.getElementById("pydos");
+    let hipodoge = document.getElementById('hipodoge');
+    let capipepo = document.getElementById('capipepo');
+    let ratigueya = document.getElementById('ratigueya');
+    let langostelvis = document.getElementById('langostelvis');
+    let tucapalma = document.getElementById('tucapalma');
+    let pydos = document.getElementById('pydos');
 
-    let spanPetPlayer = document.getElementById("pet-player");
+    let spanPetPlayer = document.getElementById('pet-player');
 
     if (hipodoge.checked) currentPetPlayer = Pets.Hipodoge;
     else if (capipepo.checked) currentPetPlayer = Pets.Capipepo;
@@ -31,29 +42,76 @@ function selectPetPlayer() {
     else if (langostelvis.checked) currentPetPlayer = Pets.Langostelvis;
     else if (tucapalma.checked) currentPetPlayer = Pets.Tucapalma;
     else if (pydos.checked) currentPetPlayer = Pets.Pydos;
-    else alert("You must select a pet ");
+    else alert('You must select a pet ');
 
     spanPetPlayer.innerHTML = currentPetPlayer;
-    
+
     selectPetEnemy();
+
+    SelectPower();
 }
 
 function selectPetEnemy() {
-    let attackRandom = random(1, 6);
-    let spanPetEnemy = document.getElementById("pet-enemy");
+    let petRandom = random(1, 6);
+    let spanPetEnemy = document.getElementById('pet-enemy');
 
-    if (attackRandom == 1) currentPetEnemy = Pets.Hipodoge;
-    else if (attackRandom == 2) currentPetEnemy = Pets.Capipepo;
-    else if (attackRandom == 3) currentPetEnemy = Pets.Ratigueya;
-    else if (attackRandom == 4) currentPetEnemy = Pets.Langostelvis;
-    else if (attackRandom == 5) currentPetEnemy = Pets.Tucapalma;
-    else if (attackRandom == 6) currentPetEnemy = Pets.Pydos;
+    if (petRandom == 1) currentPetEnemy = Pets.Hipodoge;
+    else if (petRandom == 2) currentPetEnemy = Pets.Capipepo;
+    else if (petRandom == 3) currentPetEnemy = Pets.Ratigueya;
+    else if (petRandom == 4) currentPetEnemy = Pets.Langostelvis;
+    else if (petRandom == 5) currentPetEnemy = Pets.Tucapalma;
+    else if (petRandom == 6) currentPetEnemy = Pets.Pydos;
 
     spanPetEnemy.innerHTML = currentPetEnemy;
+}
+
+function SelectPower() {
+    let buttonPyro = document.getElementById('button-pyro');
+    buttonPyro.addEventListener('click', powerPyro);
+    let buttonHydro = document.getElementById('button-hydro');
+    buttonHydro.addEventListener('click', powerHydro);
+    let buttonGeo = document.getElementById('button-geo');
+    buttonGeo.addEventListener('click', powerGeo);
+    let buttonCryo = document.getElementById('button-cryo');
+    buttonCryo.addEventListener('click', powerCryo);
+    let buttonElectro = document.getElementById('button-electro');
+    buttonElectro.addEventListener('click', powerElectro);
+
+    SetPowerEnemy();
+}
+
+function powerPyro() {
+    attackPlayer = Powers.Pyro;
+}
+
+function powerHydro() {
+    attackPlayer = Powers.Hydro;
+}
+
+function powerGeo() {
+    attackPlayer = Powers.Geo;
+}
+
+function powerCryo() {
+    attackPlayer = Powers.Cryo;
+}
+
+function powerElectro() {
+    attackPlayer = Powers.Electro;
+}
+
+function SetPowerEnemy() {
+    let powerRandom = random(0, 5);
+
+    if (powerRandom == 1) attackEnemy = Powers.Pyro;
+    else if (powerRandom == 2) attackEnemy = Powers.Hydro;
+    else if (powerRandom == 3) attackEnemy = Powers.Geo;
+    else if (powerRandom == 4) attackEnemy = Powers.Cryo;
+    else if (powerRandom == 5) attackEnemy = Powers.Electro;
 }
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-window.addEventListener("load", startGame);
+window.addEventListener('load', startGame);
